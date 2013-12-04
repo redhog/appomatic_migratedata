f564293a1470471995be9c7996686485f55e616a
Egil Moeller <egil.moller@freecode.no>
2013-12-04 11:34:11 +0100

    Initial commit

---
+++ appomatic_migratedata/management/commands/migratedata.py
@@ -0,0 +1,270 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.core.management.base import BaseCommand, CommandError
+from django.db import router, DEFAULT_DB_ALIAS
+from django.utils.datastructures import SortedDict
+import django.db.transaction
+import os
+import sys
+from optparse import make_option
+from django.conf import settings
+from django.core.management.color import no_style
+from django.db import (connections, router, transaction, DEFAULT_DB_ALIAS,
+      IntegrityError, DatabaseError)
+from django.db.models import get_apps
+from django.utils.encoding import force_text
+from django.utils._os import upath
+from itertools import product
+
+
+from optparse import make_option
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--source', action='store', dest='source',
+            default="default", help='Nominates a specific database to load from. Defaults to the "default" database.'),
+        make_option('--destination', action='store', dest='destination',
+            default="destination", help='Nominates a specific database to copy to. Defaults to the "destination" database.'),
+        make_option('-e', '--exclude', dest='exclude',action='append', default=[],
+            help='An appname or appname.ModelName to exclude (use multiple --exclude to exclude multiple apps/models).'),
+        make_option('-n', '--natural', action='store_true', dest='use_natural_keys', default=False,
+            help='Use natural keys if they are available.'),
+        make_option('-a', '--all', action='store_true', dest='use_base_manager', default=False,
+            help="Use Django's base manager to dump all models stored in the database, including those that would otherwise be filtered or modified by a custom manager."),
+        make_option('--ignorenonexistent', '-i', action='store_true', dest='ignore',
+            default=False, help='Ignores entries in the serialized data for fields'
+                                ' that do not currently exist on the model.'),
+    )
+    help = ("Copies model data from a one database to another (using each model's default manager unless --all is "
+            "specified).")
+    args = '[appname appname.ModelName ...]'
+
+    def handle(self, *args, **kwargs):
+        try:
+            return self.handle2(*args, **kwargs)
+        except Exception, e:
+            print e
+            import traceback
+            traceback.print_exc()
+
+    def handle2(self, *app_labels, **options):
+        from django.db.models import get_app, get_apps, get_model
+
+        source = options.get('source')
+        destination = options.get('destination')
+        excludes = options.get('exclude')
+        show_traceback = options.get('traceback')
+        use_natural_keys = options.get('use_natural_keys')
+        use_base_manager = options.get('use_base_manager')
+        verbosity = int(options.get('verbosity'))
+
+        excluded_apps = set()
+        excluded_models = set()
+        for exclude in excludes:
+            if '.' in exclude:
+                app_label, model_name = exclude.split('.', 1)
+                model_obj = get_model(app_label, model_name)
+                if not model_obj:
+                    raise CommandError('Unknown model in excludes: %s' % exclude)
+                excluded_models.add(model_obj)
+            else:
+                try:
+                    app_obj = get_app(exclude)
+                    excluded_apps.add(app_obj)
+                except ImproperlyConfigured:
+                    raise CommandError('Unknown app in excludes: %s' % exclude)
+
+        if len(app_labels) == 0:
+            app_list = SortedDict((app, None) for app in get_apps() if app not in excluded_apps)
+        else:
+            app_list = SortedDict()
+            for label in app_labels:
+                try:
+                    app_label, model_label = label.split('.')
+                    try:
+                        app = get_app(app_label)
+                    except ImproperlyConfigured:
+                        raise CommandError("Unknown application: %s" % app_label)
+                    if app in excluded_apps:
+                        continue
+                    model = get_model(app_label, model_label)
+                    if model is None:
+                        raise CommandError("Unknown model: %s.%s" % (app_label, model_label))
+
+                    if app in app_list.keys():
+                        if app_list[app] and model not in app_list[app]:
+                            app_list[app].append(model)
+                    else:
+                        app_list[app] = [model]
+                except ValueError:
+                    # This is just an app - no model qualifier
+                    app_label = label
+                    try:
+                        app = get_app(app_label)
+                    except ImproperlyConfigured:
+                        raise CommandError("Unknown application: %s" % app_label)
+                    if app in excluded_apps:
+                        continue
+                    app_list[app] = None
+
+        def get_objects():
+            # Collate the objects to be serialized.
+            for model in sort_dependencies(app_list.items()):
+                if model in excluded_models:
+                    continue
+                if not model._meta.proxy and router.allow_syncdb(source, model):
+                    if use_base_manager:
+                        objects = model._base_manager
+                    else:
+                        objects = model._default_manager
+                    for obj in objects.using(source).\
+                            order_by(model._meta.pk.name).iterator():
+                        yield obj
+
+
+        connection = connections[destination]
+        cursor = connection.cursor()
+
+        self.loaded_object_count = 0
+        self.fixture_object_count = 0
+        self.models = set()
+
+        transaction.commit_unless_managed(using=destination)
+        transaction.enter_transaction_management(using=destination)
+        transaction.managed(True, using=destination)
+
+        try:
+            with connection.constraint_checks_disabled():
+                for obj in get_objects():
+                    self.fixture_object_count += 1
+                    self.models.add(obj.__class__)
+                    try:
+                        obj.save(using=destination, force_insert = True)
+                        self.loaded_object_count += 1
+                        sys.stdout.write(".")
+                        sys.stdout.flush()
+                    except (DatabaseError, IntegrityError) as e:
+                        e.args = ("Could not load %(app_label)s.%(object_name)s(pk=%(pk)s): %(error_msg)s" % {
+                                'app_label': obj._meta.app_label,
+                                'object_name': obj._meta.object_name,
+                                'pk': obj.pk,
+                                'error_msg': force_text(e)
+                            },)
+                        raise
+
+            # Since we disabled constraint checks, we must manually check for
+            # any invalid keys that might have been added
+            table_names = [model._meta.db_table for model in self.models]
+            try:
+                connection.check_constraints(table_names=table_names)
+            except Exception as e:
+                e.args = ("Problem installing fixtures: %s" % e,)
+                raise
+
+        except (SystemExit, KeyboardInterrupt):
+            raise
+        except Exception as e:
+            transaction.rollback(using=destination)
+            transaction.leave_transaction_management(using=destination)
+            raise
+            
+        # If we found even one object in a fixture, we need to reset the
+        # database sequences.
+        if self.loaded_object_count > 0:
+            sequence_sql = connection.ops.sequence_reset_sql(no_style(), self.models)
+            if sequence_sql:
+                if verbosity >= 2:
+                    self.stdout.write("Resetting sequences\n")
+                for line in sequence_sql:
+                    cursor.execute(line)
+
+        transaction.commit(using=destination)
+        transaction.leave_transaction_management(using=destination)
+
+        if verbosity >= 1:
+            if self.fixture_object_count == self.loaded_object_count:
+                self.stdout.write("Installed %d object(s)" % (self.loaded_object_count,))
+            else:
+                self.stdout.write("Installed %d object(s) (of %d)" % (
+                    self.loaded_object_count, self.fixture_object_count))
+
+        # Close the DB connection. This is required as a workaround for an
+        # edge case in MySQL: if the same connection is used to
+        # create tables, load data, and query, the query can return
+        # incorrect results. See Django #7572, MySQL #37735.
+        connection.close()
+
+
+def sort_dependencies(app_list):
+    """Sort a list of app,modellist pairs into a single list of models.
+
+    The single list of models is sorted so that any model with a natural key
+    is serialized before a normal model, and any model with a natural key
+    dependency has it's dependencies serialized first.
+    """
+    from django.db.models import get_model, get_models
+    # Process the list of models, and get the list of dependencies
+    model_dependencies = []
+    models = set()
+    for app, model_list in app_list:
+        if model_list is None:
+            model_list = get_models(app)
+
+        for model in model_list:
+            models.add(model)
+            # Add any explicitly defined dependencies
+            if hasattr(model, 'natural_key'):
+                deps = getattr(model.natural_key, 'dependencies', [])
+                if deps:
+                    deps = [get_model(*d.split('.')) for d in deps]
+            else:
+                deps = []
+
+            # Now add a dependency for any FK or M2M relation with
+            # a model that defines a natural key
+            for field in model._meta.fields:
+                if hasattr(field.rel, 'to'):
+                    rel_model = field.rel.to
+                    if hasattr(rel_model, 'natural_key') and rel_model != model:
+                        deps.append(rel_model)
+            for field in model._meta.many_to_many:
+                rel_model = field.rel.to
+                if hasattr(rel_model, 'natural_key') and rel_model != model:
+                    deps.append(rel_model)
+            model_dependencies.append((model, deps))
+
+    model_dependencies.reverse()
+    # Now sort the models to ensure that dependencies are met. This
+    # is done by repeatedly iterating over the input list of models.
+    # If all the dependencies of a given model are in the final list,
+    # that model is promoted to the end of the final list. This process
+    # continues until the input list is empty, or we do a full iteration
+    # over the input models without promoting a model to the final list.
+    # If we do a full iteration without a promotion, that means there are
+    # circular dependencies in the list.
+    model_list = []
+    while model_dependencies:
+        skipped = []
+        changed = False
+        while model_dependencies:
+            model, deps = model_dependencies.pop()
+
+            # If all of the models in the dependency list are either already
+            # on the final model list, or not on the original serialization list,
+            # then we've found another model with all it's dependencies satisfied.
+            found = True
+            for candidate in ((d not in models or d in model_list) for d in deps):
+                if not candidate:
+                    found = False
+            if found:
+                model_list.append(model)
+                changed = True
+            else:
+                skipped.append((model, deps))
+        if not changed:
+            raise CommandError("Can't resolve dependencies for %s in serialized app list." %
+                ', '.join('%s.%s' % (model._meta.app_label, model._meta.object_name)
+                for model, deps in sorted(skipped, key=lambda obj: obj[0].__name__))
+            )
+        model_dependencies = skipped
+
+    return model_list
---
+++ setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+from setuptools.command import easy_install
+from setuptools import setup, find_packages
+import shutil
+import os.path
+import sys
+import hashlib
+
+PKG_DIR = os.path.abspath(os.path.dirname(__file__))
+PKG_NAME = os.path.basename(PKG_DIR)
+
+# Make it possible to overide script wrapping
+old_is_python_script = easy_install.is_python_script
+def is_python_script(script_text, filename):
+    if 'SETUPTOOLS_DO_NOT_WRAP' in script_text:
+        return False
+    return old_is_python_script(script_text, filename)
+easy_install.is_python_script = is_python_script
+
+setup(
+    name = "appomatic_migratedata",
+    description = "Shortcut for dumpdata + loaddata to migrate larger datasets between two live databases.",
+    keywords = "appomatic django database dump restore migrate",
+    install_requires = ['appomaticcore'],
+    version = "0.0.1",
+    author = "RedHog (Egil Moeller)",
+    author_email = "egil@skytruth.org",
+    license = "GPL",
+    url = "http://github.com/redhog/appomatic_migratedata",
+    packages = find_packages(),
+    package_data = {'': ['*.txt', '*.css', '*.html', '*.js']},
+    include_package_data = True
+)
